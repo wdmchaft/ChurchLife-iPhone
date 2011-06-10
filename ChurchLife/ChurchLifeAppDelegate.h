06e474122a6bf33cb511d5261d4f6279991deef6
jameymcelveen <jamey@mcelveen.us>
2011-06-09 20:14:53 -0400

    initial checkin

---
+++ ChurchLife/ChurchLifeAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ChurchLifeAppDelegate.h
+//  ChurchLife
+//
+//  Created by Jamey on 6/9/11.
+//  Copyright 2011 ACS Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChurchLifeAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
