b15e1d61802819c22c391104a9d1932d92ca605e
Michael Dropps <droppsm@gmail.com>
2011-08-01 13:38:09 -0700

    cleanup

--- ChurchLife/AcsLink.m
+++ ChurchLife/AcsLink.m
@@ -15,7 +15,7 @@
 }
 
 +(BOOL)LoginBySite: (int)siteNumber userName:(NSString *)userName password:(NSString *)password {   
-    NSURL *url = [NSURL URLWithString:@"http://secure.accessacs.com/api/account/validate"];
+    NSURL *url = [NSURL URLWithString:@"https://api.accessacs.com/account/validate"];
     NSError *error;
     NSURLResponse *response;
     NSData *dataReply;
@@ -34,10 +34,10 @@
     CurrentIdentity *identity = [CurrentIdentity sharedIdentity];
     
     if ([dict count] > 1) {
-        NSString * status = [dict objectForKey:[[dict allKeys]objectAtIndex:0]];
+        NSString * status = [dict valueForKey:@"Message"];
         if ([status isEqualToString:@"Success"])
         {
-            NSDictionary *d = [dict objectForKey:[[dict allKeys]objectAtIndex:1]];
+            NSDictionary *d = [dict objectForKey:@"Data"];
             
             identity.emailAddress = [d valueForKey:@"Email"];
             identity.siteName = [d valueForKey:@"SiteName"];
@@ -61,7 +61,7 @@
  }
 
 +(NSMutableArray *)LoginWithEmail: (NSString *)email password:(NSString *)password{
-    NSURL *url = [NSURL URLWithString:@"http://secure.accessacs.com/api/account/findbyemail"];
+    NSURL *url = [NSURL URLWithString:@"https://api.accessacs.com/account/findbyemail"];
     NSError *error;
     NSURLResponse *response;
     NSData *dataReply;
@@ -81,10 +81,10 @@
     //NSLog(@"response: %@", [decodedResponse description]);
     
     if ([decodedResponse count] > 1) {
-        NSString * status = [decodedResponse objectForKey:[[decodedResponse allKeys]objectAtIndex:0]];
+        NSString * status = [decodedResponse valueForKey:@"Message"];
         if ([status isEqualToString:@"Success"])
         {
-            NSArray *allData = [decodedResponse objectForKey:[[decodedResponse allKeys]objectAtIndex:1]];
+            NSArray *allData = [decodedResponse objectForKey:@"Data"];
             
             //NSLog(@"alldata: %@", [allData description]);
     
@@ -92,7 +92,7 @@
             
             for (int i = 0; i < [allData count]; i++)
             {
-                NSDictionary *loginData = [allData objectAtIndex:i];//[allData objectForKey:[[allData allKeys]objectAtIndex:i]];
+                NSDictionary *loginData = [allData objectAtIndex:i];
                 
                 AcsLogin *login = [AcsLogin alloc];  
                 login.siteNumber = [loginData valueForKey:@"SiteNumber"];
