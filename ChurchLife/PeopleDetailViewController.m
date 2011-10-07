b8f01ec66d3a4cbc7d03c01bbb733cc3b10973b0
Michael Dropps <droppsm@gmail.com>
2011-10-07 09:32:01 -0400

    fixed adding blank nickname to contacts

--- ChurchLife/PeopleDetailViewController.m
+++ ChurchLife/PeopleDetailViewController.m
@@ -634,7 +634,10 @@ BOOL imageLoaded;
     ABRecordSetValue(person, kABPersonPrefixProperty, indv.title, nil);
     ABRecordSetValue(person, kABPersonFirstNameProperty, indv.firstName, nil);
     ABRecordSetValue(person, kABPersonMiddleNameProperty, indv.middleName, nil);
-    ABRecordSetValue(person, kABPersonNicknameProperty, indv.goesByName, nil);
+    
+    if (![indv.goesByName isEqualToString:@""])
+        ABRecordSetValue(person, kABPersonNicknameProperty, indv.goesByName, nil);
+    
     ABRecordSetValue(person, kABPersonLastNameProperty, indv.lastName, nil);
     ABRecordSetValue(person, kABPersonSuffixProperty, indv.suffix, nil);
     
